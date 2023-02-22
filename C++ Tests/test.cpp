a3d14c4d7ce5826135096860447cb50c8b006265
streyix <paeem@dukes.jmu.edu>
2023-02-22 15:37:31 -0500

    words contains no names or words > 16

--- C++ Tests/test.cpp
+++ C++ Tests/test.cpp
@@ -1,20 +1,55 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-	char entry;
+int main() {
+    vector<string> words;
+    string word;
+    string name;
+    ifstream infile("words");
+    ifstream namefile("names");
+    ofstream outfile("words2");
 
-	cout << "Enter a value: ";
-	cin >> entry;
-	cout << endl;
+    if (!infile.is_open()) {
+        cerr << "Unable to open file." << endl;
+        return 1;
+    }
 
-	if (isalpha(entry)){
-		cout << "Alphabetical value received!" << endl;
-		return 0;
-	}
-	else{
-		cout << "Special value entered coolChamp!" << endl;
-		return 0;
-	}
+    // Read in all the words from the file
+    while (infile >> word) {
+        bool isNormal = true;
+        while (namefile >> name){
+            if (word.substr(0, name.size()) == name){
+                isNormal = false;
+                break;
+            }
+        }
+        namefile.clear();
+        namefile.seekg(0);
+        for (char c : word) {
+            if (!isalpha(c)) { // Add condition to check for non-alphabetical characters
+                isNormal = false;
+                break;
+            }
+        }
+        if (isNormal && word.find('\'') == string::npos && word.length() < 16) { // Add conditions for apostrophes and length
+            words.push_back(word);
+        }
+    }
+
+    infile.close();
+
+    // Sort the words in alphabetical order
+    sort(words.begin(), words.end());
+
+    // Print out the sorted words
+    for (const auto& w : words) {
+        outfile << w << endl;
+    }
+    cout << "DONE!" << endl;
+    return 0;
 }
